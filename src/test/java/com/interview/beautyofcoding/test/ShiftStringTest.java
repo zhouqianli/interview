d8e73924d9b19365474fd280ac9e1527575ffb6b
Zhong Zhou <zhouqianli@gmail.com>
2012-05-03 16:49:43 +0800

    Add test class for ShiftString

---
+++ src/test/java/com/interview/beautyofcoding/test/ShiftStringTest.java
@@ -0,0 +1,11 @@
+package com.interview.beautyofcoding.test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: zhouzh
+ * Date: 5/3/12
+ * Time: 4:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ShiftStringTest {
+}

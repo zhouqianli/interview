31a9810667d9c4d7d4ac252149daab5a8ef37ea3
Zhong Zhou <zhouqianli@gmail.com>
2012-05-03 17:02:42 +0800

    merge test class from branch "zhouzh"

--- src/main/java/com/interview/beautyofcoding/section_3/ShiftString.java
+++ src/main/java/com/interview/beautyofcoding/section_3/ShiftString.java
@@ -1,12 +1,9 @@
 package com.interview.beautyofcoding.section_3;
 
 /**
- * Created with IntelliJ IDEA.
- * User: zhouzh
- * Date: 5/3/12
- * Time: 3:17 PM
- * To change this template use File | Settings | File Templates.
- *
+ *  Problem:字符串移位包含问题
+ *  给定两个字符串s1和s2，要求判定s2是否能够被s1做循环移位得到的字符串包含。例如：给定s1＝aabcd 和 s2＝cdaa，返回 true；给定 s1＝abcd
+ *  和s2＝acbd，返回 false。
  */
 public class ShiftString {
 }

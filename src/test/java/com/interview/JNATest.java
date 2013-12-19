5b9e9379255551d8261b67574f322678019aa2fd
Zhong Zhou <zhouzh@yahoo-inc.com>
2013-12-19 11:25:00 +0800

    Improve code to get values from Registry.

--- src/test/java/com/interview/JNATest.java
+++ src/test/java/com/interview/JNATest.java
@@ -2,7 +2,6 @@ package com.interview;
 
 
 import com.sun.jna.platform.win32.Advapi32Util;
-import com.sun.jna.platform.win32.Win32Exception;
 import com.sun.jna.platform.win32.WinReg;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -17,30 +16,54 @@ public class JNATest {
     static final String INT_VALUE = "IntValue";
     static final String CHINESE = "张金晶出生";
 
-    public void addAndRemoveStartupKey() throws Exception {
+    public void addAndRemoveKey() throws Exception {
         Advapi32Util.registryCreateKey(
                 WinReg.HKEY_CURRENT_USER, STARTUP,
                 CHINESE);
         Advapi32Util.registrySetIntValue(
                 WinReg.HKEY_CURRENT_USER, STARTUP + "\\" + CHINESE,
                 INT_VALUE, BORN);
-        int year = getValueByKey(CHINESE);
+        int year = getIntValueByKey(CHINESE);
         Assert.assertEquals(year, BORN);
         Advapi32Util.registryDeleteKey(
                 WinReg.HKEY_CURRENT_USER, STARTUP,
                 CHINESE);
-        try {
-            getValueByKey(CHINESE);
-        } catch (Win32Exception e) {
-            Assert.assertEquals("The system cannot find the file specified.", e.getMessage());
-        }
-
+        Assert.assertFalse(isKeyExist(STARTUP + "\\" + CHINESE));
     }
 
-    private int getValueByKey(String CHINESE) {
+    public void addAndRemoveValue() throws Exception {
+        Advapi32Util.registrySetStringValue(
+                WinReg.HKEY_CURRENT_USER, STARTUP, CHINESE,
+                String.valueOf(BORN));
+        String year = getStringValueByKey(CHINESE);
+        Assert.assertEquals(year, String.valueOf(BORN));
+        Advapi32Util.registryDeleteValue(
+                WinReg.HKEY_CURRENT_USER, STARTUP,
+                CHINESE);
+        Assert.assertFalse(isValueExist(STARTUP , CHINESE));
+    }
+
+    private boolean isKeyExist(String key) {
+        return Advapi32Util.registryKeyExists(WinReg.HKEY_CURRENT_USER,
+                key);
+    }
+
+    private boolean isValueExist(String key, String value) {
+        return Advapi32Util.registryValueExists(WinReg.HKEY_CURRENT_USER,
+                key, value);
+    }
+
+    private int getIntValueByKey(String CHINESE) {
         return Advapi32Util.registryGetIntValue(
                 WinReg.HKEY_CURRENT_USER,
                 STARTUP + "\\" + CHINESE,
                 INT_VALUE);
+    }
+
+    private String getStringValueByKey(String CHINESE) {
+        return Advapi32Util.registryGetStringValue(
+                WinReg.HKEY_CURRENT_USER,
+                STARTUP,
+                CHINESE);
     }
 }
